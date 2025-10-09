becbec1139bc37ae5f2b2626eaea08a0405a084e
Richa Dwivedi <136926589+richadwivedi21@users.noreply.github.com>
2025-10-09 07:22:51 +0000

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,12 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
